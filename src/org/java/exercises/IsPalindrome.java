90596b3b4457576da1aaa58fc3a9bf1f57f82bc7
Jean <jmlrissac@gmail.com>
2023-07-17 13:42:25 +0200

    Project done.

--- src/org/java/exercises/IsPalindrome.java
+++ src/org/java/exercises/IsPalindrome.java
@@ -1,2 +1,35 @@
-package org.java.exercises;public class IsPalindrome {
+package org.java.exercises;
+
+import java.util.Scanner;
+
+public class IsPalindrome {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Inserisci una parola: ");
+        String word = scanner.nextLine();
+
+        char[] wordCharArray = word.toCharArray();
+        int length = wordCharArray.length;
+
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < length / 2; i++) {
+            String p1 = String.valueOf(wordCharArray[i]);
+            String p2 = String.valueOf(wordCharArray[length - 1 - i]);
+
+            if (!p1.equalsIgnoreCase(p2)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " è un palindromo.");
+        } else {
+            System.out.println(word + " non è un palindromo.");
+        }
+
+    }
 }
